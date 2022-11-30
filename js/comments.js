56ed16b7a2c2fa789e827967dce66883f0412ebb
Paweł <pawel_gnat@o2.pl>
2022-11-30 18:50:55 +0100

    chore: create new comment function and cleared old css styles

--- js/comments.js
+++ js/comments.js
@@ -1,4 +1,4 @@
-// const commentsContainer = document.querySelector('.comments-area')
+const sendNewCommentBtn = document.querySelector('.btn--send')
 const chatBox = document.querySelector('.chatbox')
 let loggedUser = {}
 
@@ -6,7 +6,19 @@ export function getUserInfo(user) {
 	loggedUser = {
 		name: user.username,
 		image: user.image.webp,
+		displayImage: function () {
+			displayLoggedUserImage(this.image)
+		},
 	}
+}
+
+setTimeout(() => {
+	loggedUser.displayImage()
+}, 500)
+
+function displayLoggedUserImage(img) {
+	const image = document.querySelector('.profile__user > img')
+	image.src = img
 }
 
 export function renderComments(element) {
@@ -20,7 +32,7 @@ export function renderComments(element) {
 	comment.innerHTML = createComment(element)
 	textbox.append(comment)
 
-	let replies = element.replies
+	let replies = element.replies ?? false
 
 	if (replies.length > 0) {
 		const replyArea = document.createElement('div')
@@ -40,18 +52,21 @@ export function renderComments(element) {
 	chatBox.append(textbox)
 }
 
-function createComment({ content, createdAt = now, score = 0, user, replyingTo }) {
+function createComment({ content, createdAt, score, user, replyingTo }) {
+	let image = user.image?.webp ?? loggedUser.image
+	let name = user.username ?? loggedUser.name
+
 	const commentData = `
     <div class="user-area">
         <figure>
-          <img src="${user.image.webp}" class="user-area__image" alt="" aria-hidden="true">
-          <figcaption class="user-area__username">${user.username}</figcaption>
+          <img src="${image}" class="user-area__image image" alt="" aria-hidden="true">
+          <figcaption class="user-area__username">${name}</figcaption>
         </figure>
-		${checkIfCommentIsFromLoggedUser(user)}
+		${markTheLoggedUser(name)}
         <span class="user-area__timestamp">${createdAt}</span>
     </div>
     <div class="text-area">
-		${checkIfCommentIsAReply(replyingTo)}
+		${markTheAddressee(replyingTo)}
         <span class="text-area__content">${content}</span>
     </div>
     <form class="counter-area"> 
@@ -60,14 +75,14 @@ function createComment({ content, createdAt = now, score = 0, user, replyingTo }
         <button type="button" class="counter-area__btn" aria-label="Subtract one point to a comment"><img src="./images/icon-minus.svg" alt="" aria-hidden="true"></button>
     </form>
     <div class="action-area">
-        <button type="button" class="action-area__btn btn--reply" aria-label="Reply"><img src="./images/icon-reply.svg" alt="" aria-hidden="true">Reply</button>
+		${handleCommentButtons(name)}
     </div>
     `
 
 	return commentData
 }
 
-function checkIfCommentIsAReply(reply) {
+function markTheAddressee(reply) {
 	if (reply) {
 		return `<span class="text-area__addressee">@${reply}</span>`
 	} else {
@@ -75,140 +90,39 @@ function checkIfCommentIsAReply(reply) {
 	}
 }
 
-function checkIfCommentIsFromLoggedUser(user) {
-	if (user.username === loggedUser.name) {
+function markTheLoggedUser(user) {
+	if (user === loggedUser.name) {
 		return `<span class="user-area__logged-user">you</span>`
 	} else {
 		return ''
 	}
 }
 
-// export const displayComments = comment => {
-// 	const replyBox = document.createElement('div')
-// 	replyBox.classList.add('reply-box')
+function handleCommentButtons(user) {
+	if (user === loggedUser.name) {
+		return `
+		<button type="button" class="action-area__btn btn--delete" aria-label="Delete"><img src="./images/icon-delete.svg" alt="" aria-hidden="true">Delete</button> 
+		
+		<button type="button" class="action-area__btn btn--edit" aria-label="Edit"><img src="./images/icon-edit.svg" alt="" aria-hidden="true">Edit</button>
+		`
+	} else {
+		return `<button type="button" class="action-area__btn btn--reply" aria-label="Reply"><img src="./images/icon-reply.svg" alt="" aria-hidden="true">Reply</button>`
+	}
+}
 
-// 	const newComment = document.createElement('div')
-// 	newComment.classList.add('comment')
-// 	newComment.setAttribute('role', 'comment')
+function createNewComment() {
+	const textarea = document.querySelector('.profile__textarea')
 
-// 	const createdComment = `
-//     <figure class="comment__user">
-//     <img src="${comment.user.image.png}" alt="Avatar of ${comment.user.username}" class="comment__user--image">
-//     <figcaption class="comment__user--username">${comment.user.username}</figcaption>
-//     </figure>
-//     <div class="comment__data">
-//     <p class="comment__data--createdat">${comment.createdAt}</p>
-//     </div>
-//     <div class="comment__content"><span class="comment__content--text">${comment.content}</span></div>
-//     <div class="comment__score">
-//     <button class="comment__score--icon"><img src="./images/icon-plus.svg" alt="Add one point to comment score"></button>
-//     <p class="comment__score--points">${comment.score}</p>
-//     <button class="comment__score--icon"><img src="./images/icon-minus.svg" alt="Subtract one point to comment score"></button>
-//     </div>
-//     <div class="comment__react">
-//     <button class="comment__react--reply-btn reply-btn"><img src="./images/icon-reply.svg" alt="Reply to a comment">Reply</button>
-//     </div>
-//     `
+	let newComment = {
+		content: textarea.value,
+		createdAt: 'now',
+		score: 0,
+		user: loggedUser.name,
+		replyingTo: null,
+	}
 
-// 	newComment.innerHTML = createdComment
-// 	commentsContainer.append(newComment)
-// 	commentsContainer.append(replyBox)
-// }
+	renderComments(newComment)
+	textarea.value = ''
+}
 
-// export const displayReplies = reply => {
-// 	const newReply = document.createElement('div')
-// 	newReply.classList.add('reply')
-// 	newReply.setAttribute('role', 'reply')
-// 	const createdReplyBox = Array.from(document.getElementsByClassName('reply-box'))
-
-// 	const createdReply = `
-//     <figure class="reply__user">
-//     <img src="${reply.user.image.png}" alt="Avatar of ${reply.user.username}" class="reply__user--image">
-//     <figcaption class="reply__user--username">${reply.user.username}</figcaption>
-//     </figure>
-//     <div class="reply__data">
-//     <p class="reply__data--createdat">${reply.createdAt}</p>
-//     </div>
-//     <div class="reply__content"><span class="reply__content--reply-to">@${reply.replyingTo}</span> <span class="reply__content--text">${reply.content}</span></div>
-//     <div class="reply__score">
-//     <button class="reply__score--icon"><img src="./images/icon-plus.svg" alt="Add one point to reply score"></button>
-//     <p class="reply__score--points">${reply.score}</p>
-//     <button class="reply__score--icon"><img src="./images/icon-minus.svg" alt="Subtract one point to reply score"></button>
-//     </div>
-//     <div class="reply__react">
-//     <button class="reply__react--reply-btn reply-btn"><img src="./images/icon-reply.svg" alt="Reply to a comment">Reply</button>
-//     </div>
-//     `
-
-// 	newReply.innerHTML = createdReply
-
-// 	createdReplyBox.forEach(box => {
-// 		if (box.hasChildNodes()) {
-// 			box.style.display = 'flex'
-// 		} else {
-// 			box.style.display = 'none'
-// 		}
-// 		box.append(newReply)
-// 	})
-// }
-
-// export const createComment = (image, username) => {
-// 	const newComment = document.createElement('div')
-// 	newComment.classList.add('comment')
-// 	newComment.setAttribute('role', 'comment')
-
-// 	const inputText = document.querySelector('.comments-profile__textarea--input')
-// 	let commentValue = inputText.value
-
-// 	const createdComment = `
-//     <figure class="comment__user">
-//     <img src="${image.png}" alt="Avatar of ${username}" class="comment__user--image">
-//     <figcaption class="comment__user--username">${username}</figcaption>
-//     <span class="comment__user--you">you</span>
-//     </figure>
-//     <div class="comment__data">
-//     <p class="comment__data--createdat">now</p>
-//     </div>
-//     <div class="comment__content"><span class="comment__content--text">${commentValue}</span></div>
-//     <div class="comment__score">
-//     <button class="comment__score--icon"><img src="./images/icon-plus.svg" alt="Add one point to comment score"></button>
-//     <p class="comment__score--points">0</p>
-//     <button class="comment__score--icon"><img src="./images/icon-minus.svg" alt="Subtract one point to comment score"></button>
-//     </div>
-//     <div class="comment__react">
-//     <button class="comment__react--delete-btn"><img src="./images/icon-delete.svg" alt="Delete comment">Delete</button>
-//     <button class="comment__react--edit-btn"><img src="./images/icon-edit.svg" alt="Edit comment">Edit</button>
-//     </div>
-//     `
-
-// 	newComment.innerHTML = createdComment
-// 	commentsContainer.append(newComment)
-// }
-
-// export const createReply = image => {
-// 	const newReply = document.createElement('div')
-// 	newReply.classList.add('reply')
-// 	newReply.setAttribute('role', 'reply')
-
-// 	const createdReplyBox = Array.from(document.getElementsByClassName('reply-box'))
-
-// 	const createdReply = `
-//     <form class="comments-profile">
-//     <div class="comments-profile__textarea">
-//     <textarea type="text" id="comment" class="comments-profile__textarea--input"></textarea>
-//     </div>
-//     <figure class="comments-profile__user">
-//     <img src="${image.png}" class="comments-profile__user--image">
-//     </figure>
-//     <div class="comments-profile__react">
-//     <button type="button" class="comments-profile__react--btn">reply</button>
-//     </div>
-//     </form>
-//     `
-
-// 	newReply.innerHTML = createdReply
-
-// 	createdReplyBox.forEach(box => {
-// 	box.append(newReply)
-// 	})
-// }
+sendNewCommentBtn.addEventListener('click', createNewComment)
--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 import * as commentsModule from './comments.js'
 
-// const sendNewCommentBtn = document.querySelector('.comments-profile__react--btn')
-// const replyBtns = Array.from(document.getElementsByClassName('reply-btn'))
-
-// replyBtns.forEach(btn => {
-// 	btn.addEventListener('click', e => console.log('lol'))
-// })
-
 async function getData() {
 	let response = await fetch('./data.json')
 	let data = await response.json()
@@ -20,37 +13,3 @@ async function getData() {
 	})
 }
 getData()
-
-// const getData = async () => {
-// 	let response = await fetch('./data.json')
-// 	const data = await response.json()
-// 	// console.log(data);
-// 	return data
-// }
-
-// const renderData = async () => {
-// let dataArray = await getData()
-// let allComments = dataArray.comments
-// let currentUser = dataArray.currentUser
-// const { image, username } = currentUser
-
-// allComments.forEach(element => {
-// 	let allReplies = element.replies
-// 	commentsModule.displayComments(element)
-
-// 	allReplies.forEach(element => {
-// 		commentsModule.displayReplies(element)
-// 	})
-// })
-
-// sendNewCommentBtn.addEventListener('click', e => {
-// 	let inputText = document.querySelector('.comments-profile__textarea--input').value
-
-// 	if (inputText == '') {
-// 		window.alert("Text area can't be empty")
-// 	} else {
-// 		commentsModule.createComment(image, username)
-// 	}
-// })
-// }
-// renderData()
